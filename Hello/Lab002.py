8a2c14f3c490b534d4f96e2ae9f477650418d485
codeonthegit <codeonthegit@gmail.com>
2024-07-09 14:28:55 +0530

    Continuing the python ex.

---
+++ Hello/Lab002.py
@@ -0,0 +1,14 @@
+isMale = True
+age = 65
+print(type(age))
+print(type(isMale))
+
+
+a=10
+b=20
+c=30
+print(a+b+c)
+
+x,y,z= 40,50,60
+print(x+y+z)
+
---
+++ Hello/Lab003.py
@@ -0,0 +1,5 @@
+#Take an input from the username and print it
+
+first_name=input("Enter your first name ")
+last_name=input("Enter your last name")
+print("Your name is ", first_name , last_name)
