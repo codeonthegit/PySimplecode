7608dc35d9580c84e257fc06ae2326853fca35f8
codeonthegit <codeonthegit@gmail.com>
2024-07-15 11:33:02 +0530

    Continuing the python Lab0022.

---
+++ Practice/Lab0022.py
@@ -0,0 +1,11 @@
+print("----- Start of the program -----")
+try:
+    a = int(input("\n Enter the num1"))
+    b = int(input("\n Enter the num2"))
+    c = a / b
+    print("Result Div is", c)
+except Exception as e:
+    print(e)
+    print("Please enter integer!")
+
+print("---- End of the program ----")
